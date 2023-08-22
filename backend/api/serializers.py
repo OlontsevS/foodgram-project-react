919c661fc970cbb3e561958adf7e04f1be403050
OlontsevO <olontsevsa@ya.ru>
2023-08-22 16:11:47 +0300

    isort

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -7,12 +7,8 @@ from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
 
-from foodgram.models import (Cart,
-                             Favorite,
-                             Ingredient,
-                             Recipe,
-                             RecipeIngredient,
-                             Tag)
+from foodgram.models import (Cart, Favorite, Ingredient, Recipe,
+                             RecipeIngredient, Tag)
 from users.models import Follow
 
 User = get_user_model()
