2dfb0bd6026f2e6938014e92b013236712e5d9bb
OlontsevO <olontsevsa@ya.ru>
2023-09-07 16:58:02 +0300

    removed load_data.py

--- backend/api/management/commands/load_data.py
+++ backend/api/management/commands/load_data.py
@@ -1,15 +1,15 @@
-from django.core.management.base import BaseCommand
-from foodgram.models import Ingredient
-
-
-class Command(BaseCommand):
-    help = 'Load data into db'
-
-    def handle(self, *args, **options):
-        import csv
-        with open("/data/ingredients.csv", encoding='utf8') as f:
-            reader = csv.reader(f)
-            for row in reader:
-                name = row[0]
-                measurement_unit = row[1]
-                Ingredient(name=name, measurement_unit=measurement_unit).save()
+# from django.core.management.base import BaseCommand
+# from foodgram.models import Ingredient
+#
+#
+# class Command(BaseCommand):
+#     help = 'Load data into db'
+#
+#     def handle(self, *args, **options):
+#         import csv
+#         with open("/data/ingredients.csv", encoding='utf8') as f:
+#             reader = csv.reader(f)
+#             for row in reader:
+#                 name = row[0]
+#                 measurement_unit = row[1]
+#                 Ingredient(name=name, measurement_unit=measurement_unit).save()
